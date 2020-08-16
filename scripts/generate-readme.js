7459d3254defb097d2169bb6c24e9356e39f638d
Godefroy Ponsinet <gponsinet@pm.me>
2020-08-16 13:52:47 +0200

    chore: generate readme
    Signed-off-by: Godefroy Ponsinet <gponsinet@pm.me>

---
+++ scripts/generate-readme.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node -r esm
+
+import { Octokit } from '@octokit/rest'
+const octokit = new Octokit()
+
+async function main({ env }) {
+  const repos = await octokit.repos.listForUser({
+    username: 'gponsinet',
+    private: true,
+    auth: '46c5e680510eaf911b02f14ab2e8d41fe6e6bc1b',
+  })
+  
+  console.log(repos.data.map(_ => _.name))
+}
+
+main(process)
